cc719352dd2a04b03e1564e281747f0174f05008
trindade_ <102923506+sp0ntanius@users.noreply.github.com>
2023-09-02 21:26:17 -0300

    Update cartaoProduto.js

--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -6,7 +6,7 @@ export function renderizarCatalogo() {
     for (const produtoCatalogo of catalogo) {
         
         const cartaoProduto = `<div class= 'border-solid shadow-xl shadow-stone-400/50 rounded-lg w-48 m-2 flex flex-col p-2 justify-between group ${produtoCatalogo.tipo}' id="card-produto-${produtoCatalogo.id}">
-        <img src="./ibagens/${produtoCatalogo.tipo}/${produtoCatalogo.nomeArqvImg}" 
+        <img src="./assets/${produtoCatalogo.tipo}/${produtoCatalogo.nomeArqvImg}" 
         alt="Produto ${produtoCatalogo.id}"
         class='group-hover:scale-110 duration-300 my-3 rounded-lg'
         />
